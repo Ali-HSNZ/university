cea07e271dde8c0ddf3b99bc63a64b4f3a54f9a6
Ali-HSNZ <Aliatraby@gmail.com>
2024-05-17 10:41:40 +0330

    refactor: remove unUsed static file/data

--- src/components/UI/organisms/Admin/Teachers/Classes/resources/components/Table/resources/index.ts
+++ src/components/UI/organisms/Admin/Teachers/Classes/resources/components/Table/resources/index.ts
@@ -1,4 +1,3 @@
-import STATIC_TABLE_DATA from './constants/table-data.json'
 import type IAdminTeacherClassesTableProps from './types/type'
 
-export { STATIC_TABLE_DATA, type IAdminTeacherClassesTableProps }
+export { type IAdminTeacherClassesTableProps }
--- src/components/UI/organisms/Admin/Teachers/List/resources/components/Table/resources/index.ts
+++ src/components/UI/organisms/Admin/Teachers/List/resources/components/Table/resources/index.ts
@@ -1,6 +1,5 @@
 'use client'
 import ClassAssignment from './components/ClassAssignment/ClassAssignment'
-import STATIC_CLASS_DATA from './constants/class-data.json'
 import type IAdminManageTeacherListTableProps from './types/type'
 
-export { STATIC_CLASS_DATA, ClassAssignment, type IAdminManageTeacherListTableProps }
+export { ClassAssignment, type IAdminManageTeacherListTableProps }
